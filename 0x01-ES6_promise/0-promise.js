c5152c6cf8d0a407a83556be46a2cc3d65b4cfc7
Petros Worku <31696803-petrosworku19@users.noreply.replit.com>
2024-06-25 18:20:18 +0000

    all

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,12 @@
 export default function getResponseFromAPI() {
-  return new Promise(() => {});
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous operation
+    const success = true;
+
+    if (success) {
+      resolve('Success!');
+    } else {
+      reject(new Error('Failure!'));
+    }
+  });
 }
