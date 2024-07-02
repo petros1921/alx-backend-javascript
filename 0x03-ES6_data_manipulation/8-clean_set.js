a955bfbf20caba02f32545de8906fd473c76b647
Petros Worku <31696803-petrosworku19@users.noreply.replit.com>
2024-07-02 18:15:42 +0000

    all

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,16 @@
 export default function cleanSet(set, startString) {
-  if (typeof startString !== 'string' || startString.length === 0) {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
 
-  let result = '';
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      if (result.length > 0) {
-        result += '-';
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
       }
-      result += value.slice(startString.length);
     }
   }
-
-  return result;
+  return parts.join('-');
 }
