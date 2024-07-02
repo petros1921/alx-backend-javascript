06373d100c578df2696a95925e2d7178d2a35474
Petros Worku <31696803-petrosworku19@users.noreply.replit.com>
2024-07-02 17:02:04 +0000

    all

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+
+  let result = '';
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      if (result.length > 0) {
+        result += '-';
+      }
+      result += value.slice(startString.length);
+    }
+  }
+
+  return result;
+}
