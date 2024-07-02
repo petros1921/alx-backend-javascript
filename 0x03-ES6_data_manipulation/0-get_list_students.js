0598c20dbbdaf7e14ab5e0662dc06b93f9b8257d
Petros Worku <31696803-petrosworku19@users.noreply.replit.com>
2024-07-02 14:54:02 +0000

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+// Define and Export the function getListStudents
+  export default getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
